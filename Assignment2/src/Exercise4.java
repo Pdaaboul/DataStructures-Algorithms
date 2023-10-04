2d354eeba3f5cfd6b04efb173927201756e09b98
Peter Chahid <peterdaaboul@outlook.com>
2023-10-04 15:43:26 +0300

    recurrences added

---
+++ Assignment2/src/Exercise4.java
@@ -0,0 +1,84 @@
+public class Exercise4 {
+/*
+
+- Merge Sort has a time complexity of O(n log n).
+- Binary Search have a time complexity of O(log n)
+- The twoSum Method has a time complexity of O(n log n) for the merge sort,
+and n times O(log n) for n binary searches, giving a total time complexity of O(n log n).
+-The overall time complexity of the entire algorithm is therefore O(n log n).
+ */
+
+
+    public static void main(String args[]) {
+        int[] array = {8, 2, 5, 3, 4, 7, 6, 1};
+        Exercise4 ex = new Exercise4();
+        System.out.println(ex.twoSum(array,10));
+    }
+
+    // Total runtime: O(n log n)
+    private boolean twoSum(int[] arr,int k){
+        mergeSort(arr);  // Runtime: O(n log n)
+        for (int i = 0; i < arr.length; i++) {  // Loop runs n times
+            int temp = k - arr[i];
+            if(binarySearch(arr,temp)) {  // Runtime: O(log n)
+                return true;
+            };
+        }
+        return false;
+    }
+
+    // Total runtime: O(n log n)
+    private void mergeSort(int[] array) {
+        int length = array.length;
+        if (length <= 1) return;
+        int middle = length / 2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[length - middle];
+        for(int i = 0; i < length; i++) {  // Loop runs n times
+            if(i < middle) {
+                leftArray[i] = array[i];
+            } else {
+                rightArray[i - middle] = array[i];
+            }
+        }
+        mergeSort(leftArray);  // Recursive call 1: O(n/2 log n/2)
+        mergeSort(rightArray); // Recursive call 2: O(n/2 log n/2)
+        merge(leftArray, rightArray, array);  // Merge operation: O(n)
+    }
+
+    // Runtime: O(n)
+    private static void merge(int[] leftArray, int[] rightArray, int[] array) {
+        int i = 0, l = 0, r = 0;
+        while(l < leftArray.length && r < rightArray.length) {  // Loop runs n times
+            if(leftArray[l] < rightArray[r]) {
+                array[i++] = leftArray[l++];
+            } else {
+                array[i++] = rightArray[r++];
+            }
+        }
+        while(l < leftArray.length) { array[i++] = leftArray[l++]; }
+        while(r < rightArray.length) { array[i++] = rightArray[r++]; }
+    }
+
+
+    //O(log n)
+    private boolean binarySearch(int[] arr, int key) {
+        return binarySearchHelper(arr, 0, arr.length / 2, arr.length - 1, key); //O(log n)
+    }
+
+
+  // O(log n)
+    private boolean binarySearchHelper(int[] arr, int start, int mid, int end, int key) {
+        if (end <= start) {
+            return key == arr[end];
+        }
+        if (arr[mid] == key) {
+            return true;
+        }
+        if (key > arr[mid]) {
+            return binarySearchHelper(arr, mid + 1, (end + mid) / 2, end, key);
+        } else {
+            return binarySearchHelper(arr, start, (start + mid) / 2, mid - 1, key);
+        }
+    }
+}
--- Assignment2/src/QuickSort.java
+++ Assignment2/src/QuickSort.java
@@ -1,5 +1,51 @@
+import java.util.Random;
+
 public class QuickSort {
     private int exchangeCounter = 0;
+
+
+    public static void main(String[] args) {
+        QuickSort sorter = new QuickSort();
+
+
+        int[] reversedArray = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        sorter.quickSort(reversedArray, 0, reversedArray.length - 1);
+        System.out.println("Exchanges in reversed array: " + sorter.getExchangeCounter());
+        sorter.resetExchangeCounter();
+        //Exchanges in reversed array: 109
+
+
+        int[] arrayForSmallestPivot = {45, 23, 11, 89, 77, 98, 4, 28, 65, 97, 12, 35, 64, 56, 21, 79, 10, 93, 51, 3};
+        sorter.quickSort(arrayForSmallestPivot, 0, arrayForSmallestPivot.length - 1);
+        System.out.println("Exchanges with smallest pivot: " + sorter.getExchangeCounter());
+        sorter.resetExchangeCounter();
+        //Exchanges with the smallest pivot: 41
+
+
+
+        int[] arrayForGreatestPivot = {18, 3, 21, 42, 9, 27, 53, 66, 12, 4, 19, 56, 31, 10, 8, 33, 47, 55, 29, 67};
+
+        sorter.quickSort(arrayForGreatestPivot, 0, arrayForGreatestPivot.length - 1);
+        System.out.println("Exchanges with greatest pivot: " + sorter.getExchangeCounter());
+        sorter.resetExchangeCounter();
+        //Exchanges with the greatest pivot: 66
+
+
+
+        Random rand = new Random();
+        int[] randomArray = new int[20];
+        for (int i = 0; i < 20; i++) {
+            randomArray[i] = rand.nextInt(50);
+        }
+        sorter.quickSort(randomArray, 0, randomArray.length - 1);
+        System.out.println("Exchanges in randomly sorted array: " + sorter.getExchangeCounter());
+        sorter.resetExchangeCounter();
+
+
+        //Exchanges in randomly sorted array: 50
+    }
+
+
     private  void quickSort(int[] array, int start, int end) {
 
         if(end <= start) return;
@@ -16,6 +62,7 @@ public class QuickSort {
         for(int j = start; j <= end; j++) {
             if(array[j] < pivot) {
                 i++;
+//                System.out.println("Exchange 1: Swapping array[" + i + "]=" + array[i] + " with array[" + j + "]=" + array[j]);
                 int temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
@@ -30,8 +77,12 @@ public class QuickSort {
 
         return i;
     }
-    public int getExchangeCounter() {
+    private int getExchangeCounter() {
         return exchangeCounter;
     }
+    private void resetExchangeCounter() {
+        exchangeCounter = 0;
+    }
+
 }
 
